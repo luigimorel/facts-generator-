416fd91ca67d379abbf17d1e5122efa6984dfb90
morelmiles <morelmiles@gmail.com>
2020-05-06 15:28:51 +0300

    Added the test file

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-const fact = require("index.js");
+const fact = require("./index.js");
 
+/**
+ *@params {} void
+ * returns {fact}
+ */
 const fetchFact = fact.getFact();
+
+/**
+ * Log the fact to the console
+ */
+console.log("***********************************\n\n");
+console.log(fetchFact.category);
+console.log("****************\n");
+
+console.log(fetchFact.fact);
+console.log("****************\n\n");
