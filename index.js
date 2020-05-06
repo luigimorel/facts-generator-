75afe3f0702f9403de75a3ddb6702c209b34a8c6
morelmiles <morelmiles@gmail.com>
2020-05-06 15:28:20 +0300

    Fixed the variable error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const arr = require("./facts/facts.json");
 const fact = {};
 
 randomInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 5) * min);
+  return Math.floor(Math.random() * (max - min + 3) * min);
 };
 
 module.exports.getRandomFact = function (fact) {
@@ -14,8 +14,8 @@ module.exports.getRandomFact = function (fact) {
 
 module.exports.getFact = function () {
   const index = randomInteger(0, 100);
-  fact = arr[index].fact;
-  category = arr[index].category;
+  fact.fact = arr[index].fact;
+  fact.category = arr[index].category;
 
   return fact;
 };
